52c46c94957eb91b50dc7065acd1d904bceffa9e
GB <yimeunshil@gmail.com>
2026-05-29 21:17:47 +0900

    서버 502에러 수정

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime, timedelta
@@ -174,4 +175,5 @@ def delete_session(id):
   return jsonify({"success": True})
 
 if __name__ == "__main__":
-  app.run()
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
